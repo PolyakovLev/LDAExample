44e72ca006a0299237f60593d3b7798f87209b0d
POLYAKOV Lev <Lev.POLYAKOV@raiffeisen.ru>
2019-11-29 15:51:14 +0300

    specs

---
+++ Example/Pods/Target Support Files/LDAExample/LDAExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LDAExample : NSObject
+@end
+@implementation PodsDummy_LDAExample
+@end
---
+++ Example/Pods/Target Support Files/Pods-LDAExample_Example/Pods-LDAExample_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LDAExample_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LDAExample_Example
+@end
